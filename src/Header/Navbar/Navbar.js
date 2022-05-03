8085db4c6c78aab962e4a6ab13bda8d5197af3cd
unknown <sadmansakib600@gmail.com>
2022-05-03 15:27:50 +0600

    added header with navbar banner and home

---
+++ src/Header/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm bg-secondary navbar-dark fixed-top ">
+            <div className='container'>
+                <NavLink className="navbar-brand" to='/'>IT Management</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse " id="navbarScroll">
+                    <ul className="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style={{ bsScrollHeight: "100px" }}>
+                        <li className="nav-item me-3">
+                            <Link className="nav-link active fs-5" aria-current="page" to='/'>Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/Main/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        <div className='my-5'>
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
